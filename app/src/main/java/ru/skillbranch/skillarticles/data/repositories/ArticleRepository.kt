3225dae33ad32a2390c20dd935666f1d6be8748e
Max4000 <sport4000@gmail.com>
2021-01-04 14:13:57 +0300

    8-th commit

---
+++ app/src/main/java/ru/skillbranch/skillarticles/data/repositories/ArticleRepository.kt
@@ -0,0 +1,35 @@
+package ru.skillbranch.skillarticles.data.repositories
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import ru.skillbranch.skillarticles.data.*
+
+object ArticleRepository {
+    private val local = LocalDataHolder
+    private val network = NetworkDataHolder
+
+    fun loadArticleContent(articleId: String): LiveData<List<MarkdownElement>?> {
+        return Transformations.map(network.loadArticleContent(articleId)) {
+            return@map if (it == null) null
+            else MarkdownParser.parse(it)
+        }
+    }
+
+    fun getArticle(articleId: String): LiveData<ArticleData?> {
+        return local.findArticle(articleId) //2s delay from db
+    }
+
+    fun loadArticlePersonalInfo(articleId: String): LiveData<ArticlePersonalInfo?> {
+        return local.findArticlePersonalInfo(articleId) //1s delay from db
+    }
+
+    fun getAppSettings(): LiveData<AppSettings> = local.getAppSettings() //from preferences
+
+    fun updateSettings(appSettings: AppSettings) {
+        local.updateAppSettings(appSettings)
+    }
+
+    fun updateArticlePersonalInfo(info: ArticlePersonalInfo) {
+        local.updateArticlePersonalInfo(info)
+    }
+}
---
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/View.kt
@@ -0,0 +1,34 @@
+package ru.skillbranch.skillarticles.extensions
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.marginBottom
+import androidx.core.view.marginLeft
+import androidx.core.view.marginRight
+import androidx.core.view.marginTop
+
+
+fun View.setMarginOptionally(
+    left: Int = marginLeft,
+    top: Int = marginTop,
+    right: Int = marginRight,
+    bottom: Int = marginBottom
+) {
+    val params = layoutParams as ViewGroup.MarginLayoutParams
+    params.setMargins(left, top, right, bottom)
+    this.requestLayout()
+}
+
+fun View.setPaddingOptionally(
+    left: Int = paddingLeft,
+    top: Int = paddingTop,
+    right: Int = paddingRight,
+    bottom: Int = paddingBottom
+) {
+    // ???
+    setPadding(left, top, right, bottom)
+}
+
+fun View.setPadding(padding: Int) {
+    setPadding(padding, padding, padding, padding)
+}
